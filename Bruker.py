8d35ac6c9fc58a2b91f1c66023ca5e0b9dbc2326
Scholder <olivier.scholder@empa.ch>
2017-03-30 16:01:58 +0200

    adopt PEP 8 standard to bruker.py

--- Bruker.py
+++ Bruker.py
@@ -1,57 +1,84 @@
-import numpy as np
+"""
+Module to handle SPM images recorded by a Bruker AFM
+"""
+
 import struct
 import re
+import numpy as np
 import pySPM
 
 class Bruker:
-	def __init__(self, path):
-		self.path = path
-		self.f = open(self.path, 'rb')
-		self.Layers=[]
-		self.Scanners=[]
-		mode=''
-		while True:
-			l = self.f.readline().rstrip().replace(b'\\',b"")
-			if l==b'*Ciao image list':
-				self.Layers.append({})
-				mode='Image'
-			elif l==b'*Scanner list':
-				self.Scanners.append({})
-				mode='Scanner'
-			ll=l.split(b": ")
-			if len(ll)>1:
-				if mode=='Image':
-					self.Layers[-1][ll[0]]=ll[1:]
-				elif mode=='Scanner':
-					self.Scanners[-1][ll[0]]=ll[1:]
-			if l==b"*File list end":
-				break
+    """
+    Class to handle SPM images recorded by a Bruker AFM
+    """
+    def __init__(self, path):
+        self.path = path
+        self.file = open(self.path, 'rb')
+        self.layers = []
+        self.scanners = []
+        mode = ''
+        while True:
+            line = self.file.readline().rstrip().replace(b'\\', b"")
+            if line == b'*Ciao image list':
+                self.layers.append({})
+                mode = 'Image'
+            elif line == b'*Scanner list':
+                self.scanners.append({})
+                mode = 'Scanner'
+            args = line.split(b": ")
+            if len(args) > 1:
+                if mode == 'Image':
+                    self.layers[-1][args[0]] = args[1:]
+                elif mode == 'Scanner':
+                    self.scanners[-1][args[0]] = args[1:]
+            if line == b"*File list end":
+                break
 
-	def getRawLayer(self,i):
-			off = int(self.Layers[i][b'Data offset'][0])
-			cols = int(self.Layers[i][b'Number of lines'][0])
-			rows = int(self.Layers[i][b'Samps/line'][0])
-			self.f.seek(off)
-			N=cols*rows
-			return np.array(struct.unpack("<"+str(N)+"h",self.f.read(N*2)),dtype='float64').reshape((cols,rows))
-	
-	def loadImage(self, channel, backward=False):	
-			for i in range(len(self.Layers)):
-				ln=self.Layers[i][b'@2:Image Data'][0].decode('utf8')
-				r=re.match(r'([^ ]+) \[([^\]]*)\] "([^"]*)"',ln).groups()
-				if r[2]==channel:
-					bck=False
-					if self.Layers[i][b'Line Direction'][0]==b'Retrace': bck=True
-					if bck==backward:
-						r = re.match(r'[A-Z]+ \[([^\]]+)\] \([0-9\.]+ [^\)]+\) ([0-9\.]+) V',self.Layers[i][b'@2:Z scale'][0].decode('utf8')).groups()
-						Scale=float(r[1])/65536.0
-						r=self.Scanners[0][b'@'+r[0].encode('utf8')][0].split()
-						Scale*=float(r[1])
-						Data=self.getRawLayer(i)*Scale
-						
-						zscale=r[2].split(b'/')[0]
-						s=self.Layers[i][b'Scan Size'][0].split()
-						if s[2][0]==126: s[2]=b'u'+s[2][1:]
-						Size={'x':float(s[0]),'y':float(s[1]),'unit':s[2].decode('utf8')}
-						I=pySPM.SPM_image(channel=channel,backward=backward,BIN=Data,real=Size,_type='Bruker AFM',zscale=zscale.decode('utf8'))
-						return I
+    def _get_raw_layer(self, i):
+        """
+        Internal function to retrieve raw data of a layer
+        """
+        off = int(self.layers[i][b'data offset'][0])
+        cols = int(self.layers[i][b'Number of lines'][0])
+        rows = int(self.layers[i][b'Samps/line'][0])
+        self.file.seek(off)
+        length = cols*rows
+        return np.array( \
+            struct.unpack("<"+str(length)+"h", self.file.read(length*2)), \
+            dtype='float64').reshape((cols, rows))
+
+    def load_image(self, channel, backward=False):
+        """
+        Load the SPM image contained in a channel
+        """
+        for i in range(len(self.layers)):
+            layer_name = self.layers[i][b'@2:Image data'][0].decode('utf8')
+            result = re.match(r'([^ ]+) \[([^\]]*)\] "([^"]*)"', layer_name).groups()
+            if result[2] == channel:
+                bck = False
+                if self.layers[i][b'Line Direction'][0] == b'Retrace':
+                    bck = True
+                if bck == backward:
+                    result = re.match(r'[A-Z]+ \[([^\]]+)\] \([0-9\.]+ [^\)]+\) ([0-9\.]+) V', \
+                          self.layers[i][b'@2:Z scale'][0].decode('utf8')).groups()
+                    scale = float(result[1])/65536.0
+                    result = self.scanners[0][b'@'+result[0].encode('utf8')][0].split()
+                    scale *= float(result[1])
+                    data = self._get_raw_layer(i)*scale
+
+                    zscale = result[2].split(b'/')[0]
+                    scan_size = self.layers[i][b'Scan size'][0].split()
+                    if scan_size[2][0] == 126:
+                        scan_size[2] = b'u'+scan_size[2][1:]
+                    size = { \
+                        'x':float(scan_size[0]), \
+                        'y':float(scan_size[1]), \
+                        'unit':scan_size[2].decode('utf8')}
+                    image = pySPM.SPM_image( \
+                          channel=channel, \
+                          backward=backward, \
+                          BIN=data, \
+                          real=size, \
+                          _type='Bruker AFM', \
+                          zscale=zscale.decode('utf8'))
+                    return image
