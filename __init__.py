2055ec14ad5a9a2ed76cfb3ab5a981490ed7faca
Scholder <olivier.scholder@empa.ch>
2017-04-13 15:35:23 +0200

    Add headers to package

--- __init__.py
+++ __init__.py
@@ -9,3 +9,7 @@ from pySPM.ITM import ITM
 from pySPM.collection import Collection
 
 __all__ = ["ITA","ITM","PCA","Block","SPM","Bruker","nanoscan"]
+__version__ = '0.1.1'
+__author__ = 'Olivier Scholder'
+__copyright__ = "Copyright 2017, EMPA, Dübendorf, Switzerland"
+__email__ = "olivier.scholder@empa.ch"
