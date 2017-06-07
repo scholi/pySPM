f94479d71c27e6d15a80a84180ea6f578951cd04
Scholder <olivier.scholder@empa.ch>
2017-06-07 13:19:14 +0200

    Add setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pySPM",
+    version='0.1.2',
+    description="library to handle SPM and ToF-SIMS data",
+    long_description="library to handle SPM and ToF-SIMS data",
+    url="https://github.com/scholi/pySPM",
+    author = "Olivier Scholder",
+    author_email = "olivier.scholder@empa.ch",
+    license="Apache 2.0",
+    keywords='tof sims iontof spm sfm sxm afm kpfm pca imaging ita itm',
+    packages=find_packages(exclude=['contrib','docs','tests']),
+    install_requires=['numpy','scipy','pandas','scikit-image','scikit-learn'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audiance :: Developers',
+        'Topic :: Scientific Data Processing :: Image processing',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
