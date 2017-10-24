1bf2037020002105c7b555ddb430042fe7c9072a
Scholder <olivier.scholder@empa.ch>
2017-10-24 12:29:15 +0200

    Add various mathematical functions

--- utils.py
+++ utils.py
@@ -166,9 +166,22 @@ def Lorentz(x, x0, gamma, A=None):
         return .5*gamma*R/np.pi
     return A*R*(.5*gamma)**2
 
-def CDF(x,a,x0,sig):
-    return a*stats.norm.cdf((x-x0),scale=sig)
+def CDF(x,mu,sig, lg=0):
+    from scipy.special import erf
+    g = sig*np.sqrt(2*np.log(2))
+    return lg*(.5+np.arctan2(x-mu,g)/np.pi)+(1-lg)*.5*(1+erf((x-mu)/(sig*np.sqrt(2))))
+    
+def LG(x, x0, sig=None, Amp=1, lg=.5, FWHM=None):
+    assert sig is not None or FWHM is not None
+    if FWHM is None:
+        FWHM = 2*np.sqrt(2*np.log(2))*sig
+    if sig is None:
+        sig = FWHM/(2*np.sqrt(2*np.log(2)))
+    return Amp*((1-lg)*Gauss(x,x0,sig)+lg*Lorentz(x,x0,FWHM))
 
+def logistic(x, lower=0, upper=1, growth=1, x0=0, nu=1, C=1):
+    return lower+(upper-lower)/(C+np.exp(-growth*(x-x0)))**(1/nu)    
+    
 def fitCDF1line(A):
     line = np.zeros(A.shape[1])
     for x in range(A.shape[1]):
@@ -192,7 +205,7 @@ def in_ipynb():
         return False
 
 def binning(data, N=2, axis=0, ufunc=np.sum):
-    w = int(np.ceil(data.shape[axis]/N))
+    w = int(np.floor(data.shape[axis]/N))
     r = np.copy(data)
     size = list(data.shape)
     size[axis] = w
