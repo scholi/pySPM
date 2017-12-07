2d943f88f8558b1509a98456a9256c49ab73a8bf
Olivier Scholder <olivier.scholder@empa.ch>
2017-12-07 17:37:13 +0100

    Add utils to show distance on plot

--- pySPM/utils/plot.py
+++ pySPM/utils/plot.py
@@ -7,3 +7,10 @@ def plotMask(ax, mask, color, **kargs):
     palette = copy.copy(plt.cm.gray)
     palette.set_over(color, 1.0)
     ax.imshow(m, cmap=palette, vmin=0, vmax=0.5, **kargs)
+    
+def Xdist(ax,left, right, y, color='r', linestyle=':', fmt='.2f', xtransf=lambda x: x, **kargs):
+    ax.axvline(left,color=color, linestyle=linestyle)
+    ax.axvline(right,color=color, linestyle=linestyle)
+    s = "{:"+fmt+"}"+kargs.get('unit','')
+    ax.annotate(s.format(xtransf(right-left)),(.5*(left+right),y),(0,2),textcoords='offset pixels',va='bottom',ha='center')
+    ax.annotate("",(left,y),(right,y),arrowprops=dict(arrowstyle=kargs.get('arrowstyle','<->')))
