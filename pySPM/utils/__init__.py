bce7967938f1e2dae469589f951cc818b3322e01
Olivier Scholder <olivier.scholder@empa.ch>
2018-01-25 14:54:28 +0100

    Implement accelerated_richardson_lucy

--- pySPM/utils/__init__.py
+++ pySPM/utils/__init__.py
@@ -156,3 +156,108 @@ def getToFimg(I, N=100, prog=False):
     for i in L:
         R += (np.random.rand(*I.shape)<(1-np.exp(-I)))
     return R
+    
+def getToFsimg(I, N=[10, 50, 100, 300, 500], prog=False):
+    Ns = np.insert(np.diff(N),0,N[0])
+    T = [getToFimg(I, n) for n in Ns]
+    return dict(zip(N,np.cumsum(T, axis=0)))
+
+
+def RL(x, image, psf):
+    from scipy.signal import convolve
+    I = convolve(x, psf, mode='same') # reconvoluted estimation.
+    relative_blur = np.copy(image)
+    relative_blur[I>0] = relative_blur[I>0]/ I[I>0]
+    return x * convolve(relative_blur, psf[::-1,::-1], mode='same') # Correlation is the convolution of mirrored psf
+            
+    
+def richardson_lucy(image, psf, iterations, damped=0, N=10, **kargs):
+    import scipy
+    from scipy.signal import convolve
+    
+    image = image.astype(np.float)
+    psf = psf.astype(np.float)
+    psf /= np.sum(psf) # Normalize the psf ⇒ ∫∫ psf(x,y) dx dy = 1
+    
+    # im_deconv = 0.5 * np.ones(image.shape)
+    im_deconv = np.mean(image) * np.ones(image.shape)
+            
+    # As the image and the psf are both ≥ 0, if the image value is 0, then the result should also be 0 at this position
+    im_deconv[image == 0] = 0
+
+    # Is iterations a number of a list of number?
+    dict_output = True
+    if type(iterations) is int:
+        dict_output = False
+        iterations = [iterations]
+  
+    N = max(iterations)
+        
+    results = {}
+
+    for i in range(N):
+        I = convolve(im_deconv, psf, mode='same') # reconvoluted estimation.
+        if damped == 0:            
+            im_deconv = RL(im_deconv, image, psf)
+        else:
+            ratio = np.zeros(image.shape)
+            ratio = I / image
+            logarithm = np.zeros(image.shape)
+            logarithm[ratio>0] = np.log(ratio[ratio>0])
+            U = -2*(image*logarithm-I+image)/damped**2
+            Ut = np.minimum(U, 1)
+            #relative_blur = np.ones(image.shape)
+            relative_blur = 1 + (Ut**(N-1)) * (N-(N-1)*Ut) * (image-I)/I
+            
+            im_deconv *= convolve(relative_blur, psf, mode=mode)
+        if i+1 in iterations:
+            results[i+1] = np.copy(im_deconv)
+    if dict_output:
+        return results
+    return results[N]
+
+def accelerated_richardson_lucy(image, psf, iterations):    
+    def _RL(x):
+        return RL(x, image, psf)
+    
+    image = image.astype(np.float)
+    psf   = psf.astype(np.float)
+    psf  /= np.sum(psf)
+    
+    x0 = np.mean(image) * np.ones(image.shape)
+    x0[image == 0] = 0
+    
+    x1 = _RL(x0)
+
+    h1 = x1 - x0
+    y0 = x0
+    y1 = x1
+    g0 = _RL(y0) - y0
+    g1 = _RL(y1) - y1
+        
+    dict_output = True
+    if type(iterations) is int:
+        dict_output = False
+        iterations = [iterations]
+  
+    N = max(iterations)
+        
+    results = {}
+
+    for i in range(2, N):
+        x2 = y1 + g1 # x_{k+1} = y_k + g_k
+        h2 = x2 - x1
+        a2 = np.sum(g1*g0)/np.sum(g0**2)
+        y2 = x2 + a2*h2
+        g2 = _RL(y2)-y2
+        
+        # Shift data
+        x0, x1 = x1, x2
+        y0, y1 = y1, y2
+        g0, g1 = g1, g2
+        h0, h1 = h1, h2
+        if i+1 in iterations:
+            results[i+1] = np.copy(x1)
+    if dict_output:
+        return results
+    return results[N]
