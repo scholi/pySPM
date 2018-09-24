f699a93ee019968045433d32cd7c79b8483d0bd4
Olivier Scholder <o.scholder@gmail.com>
2018-09-24 21:23:13 +0200

    Perform tests with local data

--- tests/test_ITA.py
+++ tests/test_ITA.py
@@ -1,25 +1,24 @@
 import pySPM
 import numpy as np
-from pySPM_data import get_data
 import matplotlib.pyplot as plt
 
+import os
+data = os.path.join(os.path.dirname(__file__), "AuTi_Img_Bi1_p_4_0.ita")
+
 def test_ITA_loading():
-    TOF = pySPM.ITA(get_data("BigSmiley.ita"))
-    assert len(TOF.get_masses()) == 27
-    assert TOF.Nscan == 100
+    TOF = pySPM.ITA(data)
 
 def test_SI_image():
-    TOF = pySPM.ITA(get_data("BigSmiley.ita"))
-    assert TOF.img.shape == (512, 512)
+    TOF = pySPM.ITA(data)
     assert TOF.img.shape[0] == TOF.size['pixels']['y']
     assert TOF.img.shape[1] == TOF.size['pixels']['x']
 
 def test_getAddedChannel():
-    TOF = pySPM.ITA(get_data("BigSmiley.ita"))
-    img1 = TOF.getAddedImageByMass(197)
-    img2, CH = TOF.getAddedImageByName('Au')
+    TOF = pySPM.ITA(data)
+    img1 = TOF.getAddedImageByMass(107)
+    img2, CH = TOF.getAddedImageByName('Ag', strict=True)
     assert len(CH) == 1
-    assert CH[0]['assign'] == 'Au-'
+    assert CH[0]['assign'] == 'Ag+'
     assert np.all(img1.pixels == img2.pixels)
 
 
--- tests/test_PCA.py
+++ tests/test_PCA.py
@@ -1,8 +1,10 @@
 import pySPM
 import numpy as np
-from pySPM_data import get_data
+
+import os
+data = os.path.join(os.path.dirname(__file__), "AuTi_Img_Bi1_p_4_0.ita")
 
 def test_ITAcollection():
-    C = pySPM.ITA_collection(get_data("BigSmiley.ita"))
+    C = pySPM.ITA_collection(data)
     C.runPCA()
-    assert C.PCA.loadings().shape == (25, 25)
+    assert C.PCA.loadings().shape == (19, 19)
