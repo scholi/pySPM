bc8a836b9e13e115fbcd37afcb8613e14ccc769b
dineshpinto <annual.fallout_0z@icloud.com>
2023-11-25 10:28:32 +0530

    Bugfix for version number

--- pySPM/_version.py
+++ pySPM/_version.py
@@ -1,3 +1,6 @@
 import importlib.metadata
 
-__version__ = importlib.metadata.version("pySPM")
+try:
+    __version__ = importlib.metadata.version("pySPM")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "unknown"
